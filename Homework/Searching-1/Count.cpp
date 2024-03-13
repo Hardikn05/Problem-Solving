7606b3859c19c57f300a69d2c1bf7972bfe90074
Hardikn05 <122173476+Hardikn05@users.noreply.github.com>
2024-03-13 18:54:27 +0530

    Add files via upload

---
+++ Homework/Searching-1/Count.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h> 
+using namespace std;
+
+int main()
+{
+  int n =0;
+  int q =0;  
+  cin>>n;
+  cin>>q;
+  vector<int>v(n);
+  for(int i =0;i<n;i++)
+   cin>>v[i];
+
+  sort(v.begin(),v.end());
+
+  while(q--){
+    int val;
+    cin>>val;
+    int low = 0;
+    int high = n-1;
+    int ans = n;
+    while(low<=high){
+      int mid = low+(high-low)/2;
+      if(v[mid]<val){
+        low = mid+1;
+      }
+      else{
+        ans = mid;
+        high = mid-1;
+      }
+     
+    }
+    cout<<n-ans<<endl;
+  }
+}
