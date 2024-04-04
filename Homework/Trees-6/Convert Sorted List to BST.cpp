fca70dc630a5697278c29f744bbcadb60d53ddb9
Hardikn05 <122173476+Hardikn05@users.noreply.github.com>
2024-04-04 20:46:25 +0530

    Create Convert Sorted List to BST.cpp

---
+++ Homework/Trees-6/Convert Sorted List to BST.cpp
@@ -0,0 +1,28 @@
+ class Solution {
+public:
+    TreeNode* buildTree(vector<int> &nums,int low,int high)
+    {
+        if(low<=high){
+            int mid = low + (high-low)/2;
+            TreeNode* node = new TreeNode(nums[mid]);
+            node->left = buildTree(nums,low,mid-1);
+            node->right = buildTree(nums,mid+1,high);
+            return node;
+        }
+        return NULL;
+    }
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+        int low = 0,high = nums.size()-1;
+        return buildTree(nums,low,high);
+    }
+    TreeNode* sortedListToBST(ListNode* head) {
+         vector<int> sortedArray;
+         ListNode* temp = head;
+         while(temp!=NULL)
+         {
+            sortedArray.push_back(temp->val);
+            temp=temp->next;
+         }
+         return sortedArrayToBST(sortedArray);
+    }
+};
