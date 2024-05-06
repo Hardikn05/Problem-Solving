0693dd8c9359047152f84d8793620db865c67411
Hardikn05 <122173476+Hardikn05@users.noreply.github.com>
2024-05-06 11:44:51 +0530

    Update Climbing Stairs.cpp

--- Homework/DP-1/Climbing Stairs.cpp
+++ Homework/DP-1/Climbing Stairs.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int climbStairs(int n) {
+    int spaceOptimization(int n) {
 
         if(n==1)
         return 1;
@@ -19,5 +19,36 @@ public:
         }
 
         return temp3;
+    }
+    int Memoization(int n,vector<int> &dp)
+    {
+        if(n==1 || n==2)
+        return n;
+
+        if(dp[n]!=-1)
+        return dp[n];
+
+        int onejump = Memoization(n-1,dp);
+        int twojump = Memoization(n-2,dp);
+
+        return dp[n]=onejump+twojump;
+    }
+    int Tabulation(int n)
+    {
+        vector<int> dp(n+1);
+        dp[1]=1;
+        if(n==1)
+        return 1;
+        dp[2]=2;
+        for(int i=3;i<=n;i++)
+        {
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
+    }
+    int climbStairs(int n) {
+        // vector<int> dp(n+1,-1);
+        // return Memoization(n,dp);
+        return Tabulation(n);
     }
 };
