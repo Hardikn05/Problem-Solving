b5ace0bba271c419ef1e44acb84c552a733b32c3
Hardikn05 <122173476+Hardikn05@users.noreply.github.com>
2024-05-09 12:35:12 +0530

    Update Solving Questions with brainpower.cpp

--- Homework/DP-1/Solving Questions with brainpower.cpp
+++ Homework/DP-1/Solving Questions with brainpower.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    long long solve(vector<vector<int>> &questions,int idx,vector<long long> &dp)
+    long long Memoization(vector<vector<int>> &questions,int idx,vector<long long> &dp)
     {
         if(idx>=questions.size())
         return 0;
@@ -8,16 +8,24 @@ public:
         if(dp[idx]!=-1)
         return dp[idx];
 
-        long long take = solve(questions,idx+questions[idx][1]+1,dp)+questions[idx][0];
-        long long not_take = solve(questions,idx+1,dp);
+        long long take = Memoization(questions,idx+questions[idx][1]+1,dp)+questions[idx][0];
+        long long not_take = Memoization(questions,idx+1,dp);
 
         return dp[idx]=max(take,not_take);
     }
+    // Tabulation
     long long mostPoints(vector<vector<int>>& questions) {
-        ios_base::sync_with_stdio(false);
-        cin.tie(NULL);
-        cout.tie(NULL);
-        vector<long long> dp(questions.size(),-1);
-        return solve(questions,0,dp);
+       int n=questions.size();
+        int mx=INT_MIN;
+        for(auto x:questions)
+        {
+            mx=max(mx,x[1]);
+        }
+        vector<long long> dp(n+mx+1,0);
+        for(int i=n-1;i>=0;i--)
+        {
+            dp[i]=max(questions[i][0]+dp[i+questions[i][1]+1],dp[i+1]);
+        }
+        return dp[0];
     }
 };
