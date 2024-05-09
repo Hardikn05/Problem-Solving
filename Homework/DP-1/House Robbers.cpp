6f01438bb97a2b69047f28ffc2e1dc76f1f95ef3
Hardikn05 <122173476+Hardikn05@users.noreply.github.com>
2024-05-09 11:59:49 +0530

    Update House Robbers.cpp

--- Homework/DP-1/House Robbers.cpp
+++ Homework/DP-1/House Robbers.cpp
@@ -1,5 +1,37 @@
 class Solution {
 public:
+
+int Memoization(vector<int>& nums, int idx, vector<int>& dp) {
+    if (idx >= nums.size())
+        return 0;
+
+    if (dp[idx] != -1)
+        return dp[idx];
+
+    int take = nums[idx] + Memoization(nums, idx + 2, dp);
+    int not_take = Memoization(nums, idx + 1, dp);
+
+    return dp[idx] = max(take, not_take);
+}
+int Tabulation(vector<int>& nums) {
+    int n = nums.size();
+    if (n == 0) return 0;
+    if (n == 1) return nums[0];
+    if (n == 2) return max(nums[0], nums[1]);
+    if (n == 3) return max(nums[0] + nums[2], nums[1]);
+
+    vector<int> dp(n, 0);
+    dp[0] = nums[0];
+    dp[1] = max(nums[0], nums[1]);
+    dp[2] = max(nums[0] + nums[2], nums[1]);
+
+    for (int i = 3; i < n; i++) {
+        dp[i] = max(nums[i] + dp[i - 2], dp[i - 1]);
+    }
+
+    return dp[n - 1];
+}
+    // SpaceOptimization
     int rob(vector<int>& nums) {
         int n = nums.size();
         int temp1 = nums[0];
